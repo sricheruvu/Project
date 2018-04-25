1bdbd8e26e0a94cfcc61666d77c64512e79506ad
SriCheruvu <sriram_cheruvu@yahoo.com>
2018-04-24 21:06:17 -0500

    Commiting updates

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
-## Getting and Cleaning Data Course Project - 
+## Getting and Cleaning Data Course Project 
 ## Human Activity Recognition Using Smartphones
+## Sriram Cheruvu
 
 ## The purpose of this project is to demonstrate the ability to collect, 
 ## work with, and clean a data set. The goal is to prepare tidy data that 
